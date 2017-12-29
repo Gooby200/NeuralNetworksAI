1ab6bc43169523fd64ae34d14eacd3574c73e135
Gooby200 <rgpesa@gmail.com>
2017-12-29 17:18:16 -0600

    added coin class. not done yet though

---
+++ NeuralNetworkAI/NeuralNetworkAI/Coin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkAI {
+    class Coin {
+        private int width;
+        private int height;
+        private int x;
+        private int y;
+        public Coin() : this(10, 10, 0, 0) { }
+
+        public Coin(int width, int height, int x, int y) {
+            this.width = width;
+            this.height = height;
+            this.x = x;
+            this.y = y;
+        }
+
+        private void setLocation(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        private int getX() {
+            return x;
+        }
+
+        private int getY() {
+            return y;
+        }
+    }
+}
